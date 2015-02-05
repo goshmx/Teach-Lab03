2a4e18559a354dbcaa8f0448b95e3c0d79d9946c
Gosh <osvaldocube@gmail.com>
2015-02-05 00:37:09 -0600

    Agregada interacción con Listview

--- AgendaMTI/Detalle.m
+++ AgendaMTI/Detalle.m
@@ -7,6 +7,7 @@
 //
 
 #import "Detalle.h"
+#import "AgendaList.h"
 
 NSMutableArray *datos;
 NSString *idTemp;
@@ -51,4 +52,58 @@ NSString *idTemp;
 
 - (IBAction)accionCompartir:(id)sender {
 }
+
+
+
+
+
+
+
+/**********************************************************************************************
+ Table Functions
+ **********************************************************************************************/
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [datos count];
+}
+//-------------------------------------------------------------------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 64;
+}
+//-------------------------------------------------------------------------------
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"AgendaList";
+    AgendaList *cell = (AgendaList *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil){
+        cell = [[AgendaList alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSMutableArray *dato = datos[indexPath.row];
+    cell.labelNombre.text = [dato objectAtIndex:1];
+    cell.labelEstado.text = [dato objectAtIndex:2];
+    cell.foto.image = [UIImage imageWithData:[dato objectAtIndex:4]];
+    CALayer * l = [cell.foto layer];
+    [l setMasksToBounds:YES];
+    [l setCornerRadius:30.0];
+    return cell;
+}
+
+
+//-------------------------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    
+}
+
+
 @end
