1a7e357fba64cd2deebe4880c66edb0ad06a64a2
Gosh <osvaldocube@gmail.com>
2015-02-05 00:19:53 -0600

    Agregando datos del listcell

---
+++ AgendaMTI/AgendaList.h
@@ -0,0 +1,19 @@
+//
+//  AgendaList.h
+//  AgendaMTI
+//
+//  Created by TRON on 05/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AgendaList : UITableViewCell
+//ImageView
+@property (strong, nonatomic) IBOutlet UIImageView *foto;
+@property (strong, nonatomic) IBOutlet UILabel *labelNombre;
+@property (strong, nonatomic) IBOutlet UILabel *labelEstado;
+
+
+
+@end
